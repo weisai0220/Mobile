ccfb0bc89dffa3f9df66ee856f6cc5600c053634
weisai0220 <2430460776@qq.com>
2016-05-01 03:16:51 +0800

    mobile

---
+++ index.js
@@ -0,0 +1,64 @@
+var main = document.querySelector('.main');
+var oLis = document.querySelectorAll('.main>ul>li');
+var winW = document.documentElement.clientWidth;
+var winH = document.documentElement.clientHeight;
+var desW = 640;
+var desH = 960;
+if(winW/winH<desW/desH){
+    main.style.webkitTransform = 'scale('+winH/desH+')';
+}else{
+    main.style.webkitTransform = 'scale('+winW/desW+')';
+}
+
+[].forEach.call(oLis,function(){
+    var oLi = arguments[0];
+    oLi.index = arguments[1];
+    oLi.addEventListener('touchstart',start,false);
+    oLi.addEventListener('touchmove',move,false);
+    oLi.addEventListener('touchend',end,false);
+});
+function start(e){
+    this.startX = e.changedTouches[0].pageX;
+    this.startY = e.changedTouches[0].pageY;
+}
+function move(e){
+    e.preventDefault();
+    this.flag = true;
+    var moveX = e.changedTouches[0].pageX;
+    var moveY = e.changedTouches[0].pageY;
+    var movePos = moveY - this.startY;
+    if(Math.abs(moveX-this.startX)>Math.abs(movePos)){
+        this.flag = false;
+        return;
+    }
+    var index = this.index;
+    var lastItem = oLis.length - 1;
+    [].forEach.call(oLis,function(){
+        if(index!=arguments[1]){
+            arguments[0].style.display = 'none';
+        }
+        arguments[0].className = '';
+    })
+    if(movePos>0){
+        var moveLen = -480 + movePos;
+        this.prevsIndex = index == 0 ? lastItem : index -1;
+        oLis[this.prevsIndex].style.webkitTransform = 'translateY('+moveLen+'px)';
+    }else if(movePos<0){
+        var moveLen = 480 + movePos;
+        this.prevsIndex = index == lastItem ? 0 : index+1;
+        oLis[this.prevsIndex].style.webkitTransform = 'translateY('+moveLen+'px)';
+    }
+    oLis[this.prevsIndex].style.display = 'block';
+    oLis[this.prevsIndex].className = 'zIndex';
+    this.style.webkitTransform = 'scale('+(1-Math.abs(movePos/winH)/2)+') translate(0,'+movePos+'px)';
+}
+function end(e){
+    if(this.flag){
+        oLis[this.prevsIndex].style.webkitTransform = 'translate(0,0)';
+        oLis[this.prevsIndex].style.webkitTransition = '1s';
+        oLis[this.prevsIndex].addEventListener('webkitTransitionEnd',function(){
+            this.style.webkitTransition = '';
+        },false);
+        this.flag = false;
+    }
+}
